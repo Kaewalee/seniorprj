ab311545e9ac9c78140de4e28e6dd870377358e7
Kaewalee <i.kaewalee.u@xeersoft.com>
2019-01-21 02:08:04 +0700

    'firebase'

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+		apiKey: "AIzaSyBgFjUQyW35lFlgF8Q-Fmf4HtnaKGqRbaw",
+        authDomain: "pyneproject-ad8b5.firebaseapp.com",
+        databaseURL: "https://pyneproject-ad8b5.firebaseio.com",
+        projectId: "pyneproject-ad8b5",
+        storageBucket: "pyneproject-ad8b5.appspot.com",
+        messagingSenderId: "33590640481"
+	}
+}
